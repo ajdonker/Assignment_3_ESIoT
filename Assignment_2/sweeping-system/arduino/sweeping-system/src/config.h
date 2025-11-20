b67e7ed81291298dc5552fc7b920a38e2266290a
ajdonker <blagojamu03@gmail.com>
2025-11-20 15:54:04 +0100

    transfer files to this repo

---
+++ Assignment_2/sweeping-system/arduino/sweeping-system/src/config.h
@@ -0,0 +1,10 @@
+#ifndef __CONFIG__
+#define __CONFIG__
+
+#define BT_PIN     2
+#define RED_PIN    13
+#define GREEN1_PIN 11
+#define GREEN2_PIN 12
+#define MOTOR_PIN  9
+
+#endif
---
+++ Assignment_2/sweeping-system/arduino/sweeping-system/src/model/HWPlatform.cpp
@@ -0,0 +1,50 @@
+#include "HWPlatform.h"
+#include <Arduino.h>
+#include "devices/ButtonImpl.h"
+#include "kernel/MsgService.h"
+#include "config.h"
+#include "devices/Led.h"
+#include "devices/LightSensorImpl.h"
+#include "devices/ServoMotorImpl.h"
+#include "kernel/Logger.h"
+
+void wakeUp(){}
+
+HWPlatform::HWPlatform(){
+  pButton = new ButtonImpl(BT_PIN);
+  pRedLed = new Led(RED_PIN);
+  pGreen1Led = new Led(GREEN1_PIN);
+  pGreen2Led = new Led(GREEN2_PIN);
+  pMotor = new ServoMotorImpl(MOTOR_PIN);
+}
+
+
+void HWPlatform::init(){
+}
+
+Button* HWPlatform::getButton(){
+  return this->pButton;
+}
+
+
+Led*  HWPlatform::getLed(){
+  return this->pLed;
+}
+
+ServoMotor* HWPlatform::getMotor(){
+  return this->pMotor;
+}
+
+void HWPlatform::test(){
+  bool btPressed = pButton->isPressed();
+  pLed->switchOn();
+  pMotor->on();
+  pMotor->setPosition(90);
+  Logger.log("Button: " + String(btPressed ? "pressed" : " not pressed"));
+  delay(1000);
+  pMotor->setPosition(0);
+  delay(1000);
+  pMotor->off();
+  pLed->switchOff();
+}
+
